3ea0f5612ea5344003c9dca2e51094f9aa4a6439
Neha Kumari <nehakashyap1011@gmail.com>
2024-10-11 23:51:27 +0530

    connecting database

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,4 +1,21 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "../../../lib/config/db";
+
+const LoadDB = async() => {
+    await connectDB();
+}
+
+LoadDB();
+
 export async function GET(request) {
     return NextResponse.json({msg:"get method hit"});
+}
+
+export async function POST(request) {
+    const {title, description} = await request.json();
+    await TodoModel.create({
+        title, 
+        description
+    })
+    return NextResponse.json({msg:"Todo created"});
 }
